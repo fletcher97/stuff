8e648d780040aa7e151ad511d0f04d3e37a6e269
fletcher97 <miguel.gueifao@gmail.com>
2014-11-13 22:15:25 +0000

    Create Reference.java

---
+++ Reference.java
@@ -0,0 +1,22 @@
+package nbsDiv;
+
+public class Reference {
+	public static long startTime;
+	public static long stopTime;
+	public static int nextPrime = 0;
+	public static int maxDiv = 0;
+	public static int currentNum = 0;
+	public static int recDiv = 0;
+	public static int incrementer = 10000;
+	public static int maxNum = 100;
+	public static int width = 360;
+	public static int height = 360;
+	public static int xPos = 350;
+	public static int yPos = 150;
+	
+	
+	public static int[] primes = {0,1,2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,
+		53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,
+		151,157,163,167,173,179,181,191,193,197,199};
+	
+}
