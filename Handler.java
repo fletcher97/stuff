b5faca1f0504e6aa0ae6f3feffe13b010c6b43c4
fletcher97 <miguel.gueifao@gmail.com>
2014-11-17 18:53:55 +0000

    Update Handler.java

--- Handler.java
+++ Handler.java
@@ -14,25 +14,49 @@ public class Handler implements ActionListener{
 			int x = NbsDiv.nbsDiv(Integer.parseInt(NbsDiv.text1.getText()));
 			NbsDiv.stopTimer();
 			JOptionPane.showMessageDialog(null, NbsDiv.text1.getText() + " has " + x + " divisors. It took " + NbsDiv.getTime());
-		}else if(e.getSource() == NbsDiv.button2){
+		}else if(e.getSource() == NbsDiv.button2 && !Reference.it1){
 			NbsDiv.startTimer();
 			Reference.nextPrime = 0;
 			Reference.currentNum = 0;
 			Reference.maxDiv = 0;
 			Reference.recDiv = Integer.parseInt(NbsDiv.text2.getText());
 			while(Reference.maxDiv < Reference.recDiv){
-				int temp = NbsDiv.nbsDiv(Reference.currentNum);
+				NbsDiv.nbsDiv(Reference.currentNum);
 				if(Reference.currentNum % Reference.incrementer == 0){
 					System.out.println(Reference.currentNum);
 				}
-				//System.out.println(Reference.currentNum + " has " + temp + " divisores.");
+				Reference.currentNum++;
+			}
+			int x = Reference.currentNum;
+			if(x - 1 >= 0){
+				x = Reference.currentNum - 1;
+			}
+			NbsDiv.stopTimer();
+			JOptionPane.showMessageDialog(null, "The smallest num with at least " + NbsDiv.text2.getText() + " divisors is " + x + " with " + Reference.maxDiv + ". It took " + NbsDiv.getTime());
+		}else if(e.getSource() == NbsDiv.button2 && Reference.it1){
+			NbsDiv.startTimer();
+			Reference.nextPrime = 0;
+			Reference.currentNum = 0;
+			Reference.maxDiv = 0;
+			Reference.recDiv = Integer.parseInt(NbsDiv.text2.getText());
+			while(Reference.maxDiv + 2 < Reference.recDiv){
+				NbsDiv.nbsDiv(Reference.currentNum);
+				if(Reference.currentNum % Reference.incrementer == 0){
+					System.out.println(Reference.currentNum);
+				}
 				Reference.currentNum++;
 			}
 			int x = Reference.currentNum - 1;
 			NbsDiv.stopTimer();
-			JOptionPane.showMessageDialog(null, "The smallest num with at least " + NbsDiv.text2.getText() + " divisors is " + x + " with " + Reference.maxDiv + ". It took " + NbsDiv.getTime());
+			JOptionPane.showMessageDialog(null, "The smallest num with at least " + NbsDiv.text2.getText() + " divisors is " + x + " with " + (Reference.maxDiv + 2) + ". It took " + NbsDiv.getTime());
 		}else if(e.getSource() == NbsDiv.button3){
 			Reference.incrementer = Integer.parseInt(NbsDiv.text3.getText());
+		}else if(e.getSource() == NbsDiv.button4){
+			if(Integer.parseInt(NbsDiv.text4.getText()) == 1){
+				Reference.it1 = true;
+			}else if(Integer.parseInt(NbsDiv.text4.getText()) != 1){
+				Reference.it1 = false;
+			}
 		}
 	}
 }
